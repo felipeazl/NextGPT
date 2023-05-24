f475741cb1a3db003ee46a0c1c149d1757e7fde5
felipeazl <felipelouzada671@gmail.com>
2023-05-24 18:12:12 -0300

    Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+
+import { useState } from "react";
 import { Sidebar } from "@/components/Sidebar";
 
 const Page = () => {
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+
+  const closeSidebar = () => {
+    setSidebarOpened(false);
+  };
+
+  const handleClearConversations = () => {};
+
+  const handleNewChat = () => {};
+
   return (
     <main className="flex min-h-screen bg-gpt-gray">
-      <Sidebar></Sidebar>
-      <section className="flex flex-col w-full"></section>
+      <Sidebar
+        open={sidebarOpened}
+        onClose={closeSidebar}
+        onClear={handleClearConversations}
+        onNewChat={handleNewChat}
+      >
+        ...
+      </Sidebar>
+      <section className="flex flex-col w-full">
+        <button onClick={() => setSidebarOpened(true)}>Abrir Sidebar</button>
+      </section>
     </main>
   );
 };
